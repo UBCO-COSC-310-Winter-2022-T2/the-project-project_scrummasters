8d7f6504c04b53c9ef139618ef733c8eab6254da
Gerard Escolano <gerard02@student.ubc.ca>
2023-04-03 11:52:47 -0600

    created the navigation bar, added it to home

---
+++ src/php/includes/navigation.php
@@ -0,0 +1,64 @@
+<?php
+switch ($_SERVER["SCRIPT_NAME"]) {
+    case "/Githubrepos/the-project-project_scrummasters/src/php/views/home.php":
+        $CURRENT_PAGE = "Home";
+        break;
+    case "/Githubrepos/the-project-project_scrummasters/src/php/views/friends.php":
+        $CURRENT_PAGE = "friends";
+        $PAGE_TITLE = "Contact Us";
+        break;
+    default:
+        $CURRENT_PAGE = "None";
+}
+?>
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        border: 1px solid #e7e7e7;
+        background-color: #f3f3f3;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: #666;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover:not(.active) {
+        background-color: #ddd;
+    }
+
+    li a.active {
+        color: white;
+        background-color: #04AA6D;
+    }
+</style>
+
+<div class="container">
+
+    <ul "nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link <?php if ($CURRENT_PAGE == "Home") {?>active<?php }?>" href="../views/home.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?php if ($GLOBALS["CURRENT_PAGE"] == "friends") {?>active<?php }?>" href="../views/home.php">Friends</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?php if ($GLOBALS["CURRENT_PAGE"] == "AddFriends") {?>active<?php }?>" href="../views/home.php">Account</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?php if ($GLOBALS["CURRENT_PAGE"] == "AddFriends") {?>active<?php }?>" href="../views/home.php">Server</a>
+        </li>
+    </ul>
+</div>
+
+
--- src/php/tests/test.php
+++ src/php/tests/test.php
@@ -7,6 +7,15 @@ class phpUnitTest extends TestCase
         $this->assertTrue(true);
     }
 
+    public function testCreateUser(){
+        require_once 'SignUpFormGetter.php';
+        require_once 'SignUpFormInserter.php';
+        $signUpFormGetter = new SignUpFormGetter();
+        $signUpFormInserter = new SignUpFormInserter($signUpFormGetter);
+        $signUpFormInserter->insert();
+        header("Location: ../../../html/loginform.html");
+    }
+
     public function testSimpleFalse()
     {
         $this->assertFalse(true);
