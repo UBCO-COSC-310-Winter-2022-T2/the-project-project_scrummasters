d5b10d0ea0dd2c9fb664da64386f01b4706cb8b1
Gerard Escolano <gerard02@student.ubc.ca>
2023-04-03 12:15:51 -0600

    created the navigation bar, added it to home

--- src/php/tests/test.php
+++ src/php/tests/test.php
@@ -7,14 +7,6 @@ class phpUnitTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function testCreateUser(){
-        require_once 'SignUpFormGetter.php';
-        require_once 'SignUpFormInserter.php';
-        $signUpFormGetter = new SignUpFormGetter();
-        $signUpFormInserter = new SignUpFormInserter($signUpFormGetter);
-        $signUpFormInserter->insert();
-        header("Location: ../../../html/loginform.html");
-    }
 
     public function testSimpleFalse()
     {
