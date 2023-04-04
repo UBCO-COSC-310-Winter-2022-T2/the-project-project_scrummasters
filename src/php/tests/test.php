6c983559d0d2c3fd16a6bd4b5c99e1bf073acb5b
Gerard Escolano <gerard02@student.ubc.ca>
2023-04-04 17:58:02 -0600

    created the setUp and teardDown organization

--- src/php/tests/test.php
+++ src/php/tests/test.php
@@ -2,6 +2,49 @@
 use PHPUnit\Framework\TestCase;
 class phpUnitTest extends TestCase
 {
+    //setup create
+    protected function setUp(): void
+    {
+
+    }
+
+    protected function tearDown(): void
+    {
+        unset($_POST['username']);
+        unset($_POST['password']);
+        unset($_POST['email']);
+        unset($_POST['phoneNumber']);
+        unset($_POST['firstName']);
+        unset($_POST['lastName']);
+
+        //deleteUserFromDatabase
+
+    }
+
+    //tearDown delete
+
+
+    public function testRegistrationNew(){
+        $_POST['username'] = 'gerry';
+        $_POST['password'] = 'gerry';
+        $_POST['email'] = 'gerry@gmail.com';
+        $_POST['phoneNumber'] = '7864516918';
+        $_POST['firstName'] = 'gerry';
+        $_POST['lastName'] = 'gerry';
+
+
+
+        require_once 'SignUpFormGetter.php';
+        require_once 'SignUpFormInserter.php';
+        $signUpFormGetter = new SignUpFormGetter();
+        $signUpFormInserter = new SignUpFormInserter($signUpFormGetter);
+        $signUpFormInserter->insert();
+    }
+
+    public function testRegistrationExisting(){
+        //create and delete
+        $_POST['username'] = 'gerry';
+    }
     public function testSimpleTrue()
     {
         $this->assertTrue(true);
