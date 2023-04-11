7ca270daeda171ae6e63019ff9162d4cfef35194
Gerard Escolano <gerard02@student.ubc.ca>
2023-04-11 13:49:26 -0600

    showing server messages

--- src/php/views/serverPage.php
+++ src/php/views/serverPage.php
@@ -3,6 +3,53 @@
 <head>
 </head>
 <body>
-This is the server page
+<style>
+    html, body{
+        background-color: #23272a;
+
+    }
+     .vertical-menu {
+         display: block;
+     }
+    .vertical-menu a {
+        display: block;
+        color: #cccccc;
+    }
+</style>
+
 
 <?php
+session_start();
+if (empty($_SESSION["username"])) {
+    header("Location: ../loginform.php");
+    exit();
+}
+$serverID = $_GET['serverID'];
+require_once '../server/serverInfoGetter.php';
+$serverInfoGetter = new serverInfoGetter($serverID);
+$connection = $serverInfoGetter->connection;
+
+$sql = 'SELECT * FROM servermessage WHERE serverID = ' . $serverID . ' ';
+$result = mysqli_query($connection, $sql);
+echo('<div class="vertical-menu">');
+if ($result && $result->num_rows > 0) {
+    // Loop through each row in the result
+    while ($row = $result->fetch_assoc()) {
+        // Get the value of the column you want to use for the link
+        $messageID = $row['serverMessageID'];
+        $message = $row['serverMessage'];
+        $senderUsername = $row['senderUsername'];
+        echo("<a> $message </a>");
+
+
+    }
+
+    // Free the result set
+    $result->free();
+} else {
+    echo "BE THE FIRST ONE TO SEND A MESSAGE!";
+}
+
+
+
+echo $serverID;
