f6ae6fcd76b81a404f82f846dfabbec81c6030ad
Shaheer <shaheershoaib11@gmail.com>
2023-04-13 10:35:03 -0700

    servers and corresponding chat on home, able to mention users

--- src/php/includes/serverList.php
+++ src/php/includes/serverList.php
@@ -1,7 +1,5 @@
 <style>
-    .vertical-menu {
-        width: 100px;
-    }
+
 
     .vertical-menu li {
         background-color: #23272a;
@@ -24,19 +22,27 @@
         font-size: 16px;
     }
 
+    button:hover{
+        cursor: pointer;
+    }
+
     .vertical-menu li:hover {
         background-color: #ccc;
     }
 
 </style>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <?php
 
 
 // Connect to your MySQL database
 
+session_start();
 if(empty($_SESSION["username"]))
 {
-    header("Location: ../loginform.php");
+    header("Location: ../views/loginform.php");
     exit();
 }
 
@@ -49,7 +55,7 @@ $connection = $dbConnection->getConnection();
 
 
 // Query your database
-$sql = "SELECT * FROM userinserver WHERE username = \"$username\"";
+$sql = "SELECT * FROM userInServer JOIN discordServer USING(serverID) WHERE username = \"$username\"";
 
 $result = mysqli_query($connection, $sql);
 
@@ -66,9 +72,12 @@ if ($result && $result->num_rows > 0) {
 
 
         // Create the link using the value
+?>
 
-        echo '<li><a href="../views/serverPage.php?serverID='.$linkValue.' " target="_self"><button>'.$linkValue.'</button></a></li>';
+     <li><button serverID = <?php echo $linkValue; ?>> <?php echo $row["serverName"]; ?> </button></a></li>
+<?php
     }
+
 
     // Free the result set
     $result->free();
@@ -80,6 +89,15 @@ echo("</ul></div>");
 // Close the database connection
 
 ?>
-</body>
-</html>
+
+<script>
+
+    $("button").on("click", function(){
+    const serverID = $(this).attr("serverID");
+    $.get("../views/serverPage.php", {serverID: serverID}, function(data){
+        $("#content").html(data);
+    });
+    });
+</script>
+
 
--- src/php/views/serverPage.php
+++ src/php/views/serverPage.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <html>
 <head>
 </head>
@@ -9,12 +10,46 @@
 
     }
      .vertical-menu {
-         display: block;
+         height: 100%;
+         display: flex;
+         flex-direction: column;
      }
-    .vertical-menu a {
-        display: block;
+    .vertical-menu p {
+
         color: #cccccc;
     }
+
+    .message{
+        display: inline-block;
+        margin-top: 10px;
+        background-color: #1f2c33;
+        padding: 10px;
+        border-radius: 20px;
+    }
+    p.senderUsername{
+        color:#05aa6d;
+    }
+    #messages{
+        overflow-y: scroll;
+        margin-left: 10px;
+        height: 90%;
+    }
+
+    #msgBoxContainer{
+        height: 10%;
+        background-color: #1e1f22;
+        margin-top: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #msgBox{
+        width: 50%;
+        height: 50%;
+        border-radius: 20px;
+    }
+
 </style>
 
 
@@ -27,8 +62,8 @@ if (empty($_SESSION["username"])) {
 }
 //get server id from the link
 $serverID = $_GET['serverID'];
+$username = $_SESSION["username"];
 require_once '../server/serverInfoGetter.php';
-
 //get information on the server and connect to db
 $serverInfoGetter = new serverInfoGetter($serverID);
 $_POST["serverID"] = $serverID;
@@ -38,7 +73,7 @@ $connection = $serverInfoGetter->connection;
 $sql = 'SELECT * FROM servermessage WHERE serverID = ' . $serverID . ' ';
 $result = mysqli_query($connection, $sql);
 echo('<div class="vertical-menu">');
-
+echo ('<div id = "messages">');
 if ($result && $result->num_rows > 0) {
     // Loop through each row in the result
     while ($row = $result->fetch_assoc()) {
@@ -46,26 +81,53 @@ if ($result && $result->num_rows > 0) {
         $messageID = $row['serverMessageID'];
         $message = $row['serverMessage'];
         $senderUsername = $row['senderUsername'];
-        echo("<a> $message </a>");
+
+        echo ('<div class = "message">');
+        echo('<p class = "senderUsername">'. $senderUsername. '</p>');
+        echo('<p class = "msgText">'. $message .'</p>');
+        echo('</div>');
+        echo ('<br>');
+
 
 
     }
-    echo('</div>');
+    echo ('</div>');
+    ?>
 
+    <div id = "msgBoxContainer">
+        <br>
+        <input type = "text" name = "message" placeholder = "Enter your message here" required id = "msgBox">
+    </div>
+
+    </div>
+
+<?php
     // Free the result set
     $result->free();
-} else {
+} else
     echo "BE THE FIRST ONE TO SEND A MESSAGE!";
-}
 ?>
-<?php
-//make the button call for the send message page
-echo ("<form method = \"post\" action = \"../server/sendMessage.php?serverID=$serverID\">");
-?>
-    <div class = "input-field">
-        <br>
-        <input type = "text" name = "message" placeholder = "Enter your message here" required>
-    </div>
-    <button>Send</button>
-</form>
+
+
+
 </body>
+
+<script>
+    $(document).ready(function(){
+        $(".msgText").filter(function(){
+            var msgTextSplit = $(this).text().split(" ");
+            console.log(msgTextSplit[0]);
+            for(let i=0; i<msgTextSplit.length; i++)
+            {
+                if(msgTextSplit[i] == "@<?php echo $username; ?>")
+                {
+                    console.log(msgTextSplit[0]+" is true");
+                    return true;
+                }
+            }
+            return false;
+        }).css("color", "gold");
+    })
+
+
+</script>
